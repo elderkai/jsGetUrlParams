17ab5c92ecb85ac23920ce9a55806d68a613d74b
elderkai <2498695596@qq.com>
2020-01-11 15:02:21 +0800

    1

---
+++ geturl.js
@@ -0,0 +1,18 @@
+let url="http://localhost:8080/article?id=5e10662d544bb020845974dd&name=sadfsd"
+function getParams(url){
+    let result={}
+    let index=url.indexOf("?")
+    if(index==-1){
+        return result
+    }else{
+        url=url.substring(index+1).split("&")
+    }
+url.forEach(index => {
+    index=index.split("=")
+  
+    result[index[0]]=index[1]
+});
+return result
+}
+console.log(getParams(url));
+
